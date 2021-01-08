1daabc02ae7202332d721b4a20c7c27fbab6efe4
Teune <teune2000@hotmail.com>
2021-01-08 16:58:19 +0100

    moment

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -34,8 +34,10 @@ window.addEventListener('storage', function (e) {
   }
 })
 
-const now = moment()
-now.add(1, 'year').subtract(20, 'days')
-console.log(now.format('MMMM Do YYYY'))
+// const now = moment()
+// now.subtract(1, 'week').subtract(20, 'days')
+// console.log(now.format('MMMM Do YYYY'))
+// console.log(now.fromNow())
+// const nowTimestamp = now.valueOf()
 
-
+// console.log(moment(nowTimestamp).toString())
